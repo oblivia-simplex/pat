ae49443df0dff0c6c2e93297c8314b077d83b38d
oblivia simplex <lucca.fraser@gmail.com>
2016-10-29 19:14:34 -0300

    now able to find offset of string in pattern, and generate indefinitely long patterns

--- pat.py
+++ pat.py
@@ -2,28 +2,40 @@
 
 import sys
 
-def main(length):
-  upper   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  lower   = upper.lower()
-  digit   = '0123456789'
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lower = upper.lower()
+digit = '0123456789'
+
+def make_pat(length, find=None):
   count   = 0
   out     = ''
   for u in upper:
     for l in lower:
       for n in digit:
-        out += u+l+n
+        chunk = u+l+n
+        out += chunk
+        if find is not None:
+          if find in out[-(2*len(find)):]:
+            return out.index(find)
         count += 3
         if count >= length:
-          return out[:length]
+          if find is None:
+            return out[:length]
+          else:
+            return 'NOT FOUND'
+  return out+make_pat(length-count, find=find)
 
 def usage():
-  print('Usage: {:s} <length>'.format(sys.argv[0]))
+  print('Usage: {:s} (<length>|<string to find>)'.
+                  format(sys.argv[0]))
 
 
 if __name__ == '__main__':
   try:
     length = int(sys.argv[1])
-    print(main(length))
-  except (ValueError, IndexError):
+    print(make_pat(length))
+  except (ValueError):
+    print(make_pat(length=20280, find=sys.argv[1]))
+  except (IndexError):
     usage()
 
