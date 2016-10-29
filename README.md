e7ef8170b29dd68cb037a05bb216706f649acdf6
oblivia simplex <lucca.fraser@gmail.com>
2016-10-29 15:13:49 -0300

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# pat
-pat the pattern generator! just a simplified, and much faster, version of metasploit's create_pattern.rb
---
+++ pat.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys
+
+def main(length):
+  upper   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  lower   = upper.lower()
+  digit   = '0123456789'
+  count   = 0
+  out     = ''
+  for u in upper:
+    for l in lower:
+      for n in digit:
+        out += u+l+n
+        count += 3
+        if count >= length:
+          return out[:length]
+
+def usage():
+  print('Usage: {:s} <length>')
+
+
+if __name__ == '__main__':
+  try:
+    length = int(sys.argv[1])
+    print(main(length))
+  except (ValueError, IndexError):
+    usage()
+
