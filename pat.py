101f1a73c10e30cb0d2f93753b55fd3439dd013a
oblivia simplex <lucca.fraser@gmail.com>
2016-10-29 20:48:01 -0300

    better offset-finding algorithm (O(1) instead of a sloppy-ass O(n))

--- pat.py
+++ pat.py
@@ -7,22 +7,16 @@ upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lower = upper.lower()
 digit = '0123456789'
 
-def pat(length, find=None):
+def pat(length):
   count   = 0
   out     = ''
   for u in upper:
     for l in lower:
       for n in digit:
-        chunk = u+l+n
-        out += chunk
-        if find is not None and find in out[-(2*len(find)):]:
-          return out.index(find)
+        out += u+l+n
         count += 3
         if count >= length:
-          if find is None:
-            return out[:length]
-          else:
-            return 'NOT FOUND'
+          return out[:length]
   try:
     return out+pat(length-count, find=find)
   except RecursionError:
@@ -32,6 +26,27 @@ def usage():
   print('Usage: {:s} (<length>|<string to find>)'.
                   format(sys.argv[0]))
 
+def find(p):
+  # assume that p is Uld
+  # we'll adjust later
+  adj = 0
+  u, l, d = '', '', ''
+  D = [c for c in p if c in digit]
+  L = [c for c in p if c in lower]
+  U = [c for c in p if c in upper]
+  if not (D and L and U):
+    return 'NOT ENOUGH INFORMATION TO PINPOINT OFFSET'
+  else:
+    u, l, d = U[0], L[0], D[0]
+  if p.index(d) < p.index(u):
+    adj = -1
+    d = digit[(digit.index(d) + 1) % 10]
+  if p.index(l) < p.index(u):
+    adj = -2
+  place = ((upper.index(u) * 26*10) + 
+           (lower.index(l) * 10)    +
+           (digit.index(d)))        * 3
+  return place + adj
 
 if __name__ == '__main__':
   try:
@@ -45,7 +60,7 @@ if __name__ == '__main__':
     length = int(sys.argv[1])
     print(pat(length), end=end)
   except (ValueError):
-    print(pat(length=20280, find=sys.argv[1]))
+    print(find(sys.argv[1]))
   except (IndexError):
     usage()
 
